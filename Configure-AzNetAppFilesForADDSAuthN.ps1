4b72e91a30e7f9b36881b4ea28d8dfdb43467a0b
Ken Hoover <kenhoover@microsoft.com>
2020-12-02 12:54:39 -0500

    Initial push

---
+++ Configure-AzNetAppFilesForADDSAuthN.ps1
@@ -0,0 +1,327 @@
+# Configure-AzFilesForADDSAuthN.ps1
+
+# Original version by John Kelbley <johnkel at Microsoft dotcom>
+# Updated by Ken Hoover <kenhoover at Microsoft dotcom>
+
+###############################################################################################################
+#  This is the "Manual" process to configure AD authentication for Azure Files
+#  (automated!)
+#
+#  Assumes:
+#	* You are running this script as a user that can create a new object in the target AD
+#   * You have the "Az" and "AzureADPreview" modules installed
+#   * You are connected via Connect-AzAccount as a user with "owner" rights to manually add roles in portal...and a share!
+#   * You have connected to the target AzureAD environment using Connect-AzureAD
+#	* An OU in the local AD has been identified for the new computer object
+#	* You have AAD synced to AD (have you confirmed this is working?)
+#	* You are executing this script on a domain joined VM as a user with computer account add rights
+# 
+# Process here:  https://docs.microsoft.com/en-us/azure/storage/files/storage-files-identity-auth-active-directory-enable
+#
+###############################################################################################################
+#
+############################################################################
+# Required parameters - make sure you have all of this info ahead of time
+############################################################################
+#
+
+param(
+    [Parameter(mandatory = $true)][string]$storageAccountName,      # The name of the storage account with the share
+    [Parameter(mandatory = $true)][string]$storageAccountRGName,    # The name of the RG the storage account is in
+    [Parameter(mandatory = $true)][string]$profileShareName,        # The name of the profiles share.  The share will be created if it doesn't exist.
+    [Parameter(mandatory = $true)][string]$ADOuDistinguishedName,   # The full DN of the OU to put the new computer object in
+    [Parameter(mandatory = $true)][string]$ShareAdminGroupName,     # the name of an AD group which will have elevated access to the profile share
+    [Parameter(mandatory = $true)][string]$ShareUserGroupName,      # the name of an AD group which will have normal access to the profile share
+    [Parameter(mandatory = $false)][switch]$IsGovCloud              # MUST add this parameter if you're working in Azure Gov Cloud, otherwise don't use it
+)
+
+# Some sanity checks before we get started
+
+# Make sure we are connected to Azure
+$currentContext = Get-AzContext -ErrorAction SilentlyContinue
+if ($null -eq $currentContext) {
+    write-warning ("You must connect to Azure with Connect-AzAccount before running this script.")
+    exit
+}
+
+# Make sure we're connected to Azure AD
+if ($null -eq (Get-AzureADCurrentSessionInfo)) {
+    write-output "You must connect to AzureAD using Connect-AzureAD before running this script."
+    exit
+}
+
+# Storage account name needs to be < 15 characters to avoid risk of hitting legacy NetBIOS limits in AD
+if ($storageAccountName.Length -ge 15) {
+    write-warning ("Storage account name (" + $storageAccountName.Length + ") is over 15 characters.  Please use a shorter name to avoid issues.")
+    exit
+}
+
+
+# Start by confirming that the storage account specified actually exists.
+write-verbose ("Verifying that we can access $storageAccountName" )
+if (Get-AzStorageAccount -name $storageAccountName -resourceGroupName $storageAccountRGName) {
+# Create a Kerb key for the storage account to use with ADDS
+    write-verbose ("Creating Kerberos key for storage account $storageAccountName")
+    $newkey = New-AzStorageAccountKey -ResourceGroupName $storageAccountRGName -name $storageAccountName -KeyName kerb1
+    $Keys = get-azstorageaccountkey -ResourceGroupName $storageAccountRGName -Name $storageAccountName -listkerbkey
+    $kerbkey = $keys | where-object {$_.keyname -eq 'kerb1'} 
+    $CompPassword = $kerbkey.value | ConvertTo-Securestring -asplaintext -force
+} else {
+    Write-Warning ("s\Storage account $storageAccountName not found.")
+    exit
+}
+
+#################################
+#Step 2 Create Computer Account and SPN, and get AD information
+#################################
+
+# AD Settings - You will want to run to following to get all the parameters you need:
+$Forest = get-adforest
+$Domain = get-ADdomain
+
+if ((!($Forest)) -or (!($Domain))) {
+    write-error ("Unable to contact AD. Exiting.")
+    exit
+}
+
+# For Azure Commercial
+# SPN should look like:		cifs/your-storage-account-name-here.file.core.windows.net	
+# For Gov looks like:		cifs/your-storage-account-name-here.file.core.usgovcloudapi.net 
+#
+if ($isGovCloud)  {
+	$SPN = "cifs/$storageAccountName.file.core.usgovcloudapi.net" 
+} Else { 
+	$SPN = "cifs/$storageAccountName.file.core.windows.net" 
+}
+
+Write-Verbose "SPN for new account will be $SPN"
+
+if (Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountRGName) {
+
+    # Make sure the target OU DN actually exists just to make sure
+    $OUlist = get-adobject -filter 'ObjectClass -eq "organizationalUnit"'
+    if ($oulist.distinguishedName -contains $ADOuDistinguishedName) {
+        if (get-ADComputer -Filter { Name -eq '$storageAccountName' } -ErrorAction SilentlyContinue) {
+            write-verbose ("Computer object $computeraccountName already exists in AD.")
+        } else {
+            write-verbose ("Creating computer object in AD...")
+            $result = New-ADComputer -name $storageAccountName -path $ADOUDistinguishedName `
+                -Description "DO NOT DELETE - Azure File Share" `
+                -ServicePrincipalNames $SPN `
+                -PasswordNeverExpires $true `
+                -OperatingSystem "Azure Files" `
+                -AccountPassword $CompPassword
+        }
+    } else {
+        write-warning ("OU `"$ADOuDistinguishedName`" not found.")
+        exit
+    }
+} else {
+    write-warning ("Storage account $storageAccountName not found in RG $storageAccountRGName. Exiting")
+}
+
+$Computer = get-ADComputer $storageAccountName  # The computer object in AD for the share.
+
+#if you fail to create the account, it could be rights (often), but commonly the DN for the OU is wrong (folks guess at it!)
+# check to see what the DN actually is either in ADUC or pull all the OUs with the following command:
+# 	get-adobject -filter 'ObjectClass -eq "organizationalUnit"'
+
+#################################
+#Step 3 update Storage account
+#################################
+#
+# Set the feature flag on the target storage account and provide the required AD domain information
+write-verbose ("Configuring storage account $storageAccountName for ADDS Authentication...")
+$updateresult = Set-AzStorageAccount `
+        -ResourceGroupName $storageAccountRGName `
+        -Name $storageAccountName `
+        -EnableActiveDirectoryDomainServicesForFile $true `
+        -ActiveDirectoryDomainName $Domain.dnsroot `
+        -ActiveDirectoryNetBiosDomainName $Domain.netbiosname `
+        -ActiveDirectoryForestName $Forest.name `
+	    -ActiveDirectoryDomainGuid $Domain.ObjectGUID `
+        -ActiveDirectoryDomainsid $Domain.DomainSID `
+        -ActiveDirectoryAzureStorageSid $Computer.sid
+
+if (!($updateresult)) {
+    write-warning "error occurred while updating the storage account.  Exiting."
+}
+
+#################################
+#Step 4 Confirm settings
+#################################
+
+#
+# Get the target storage account
+$storageaccount = Get-AzStorageAccount -ResourceGroupName $storageAccountRGName -Name $storageAccountName
+
+if (($storageaccount.AzureFilesIdentityBasedAuth.DirectoryServiceOptions -eq "AD") -and `
+    ($storageaccount.AzureFilesIdentityBasedAuth.ActiveDirectoryProperties.DomainName)) {
+    write-verbose ("Storage account $storageAccountName is configured to use domain " + ($storageaccount.AzureFilesIdentityBasedAuth.ActiveDirectoryProperties.DomainName + " for authentication."))
+} else {
+    write-warning ("Storage account configuration does not match expectations.  Please check and try again.")
+    exit 
+}
+
+
+#####################################################
+#Step 5 Set Azure Roles (share) security
+#
+#####################################################
+#
+################################################################################
+#
+#  Add Share-level permissions as per https://docs.microsoft.com/en-us/azure/storage/files/storage-files-identity-ad-ds-assign-permissions
+#
+#  Users should have "Storage File Data SMB Share Contributor" and Admins "Storage File Data SMB Share Elevated Contributor"  
+#
+
+Write-verbose ("Setting storage context for role assignment...")
+
+$storageContext = New-AzStorageContext -StorageAccountName $storageAccountName `
+                        -StorageAccountKey (get-AzStorageAccountKey -ResourceGroupName $storageAccountRGName `
+                        -Name $storageAccountName)[0].Value
+
+# Check if the share exists.  If not, create it.
+Write-Verbose ("Checking if share $profileShareName exists in storage account $storageAccountName...")
+if (!(get-AzStorageShare -Name $profileShareName -Context $storageContext -ErrorAction SilentlyContinue)) {
+    write-verbose ("File share $profileShareName does not exist.  Creating new share $profileShareName...")
+    New-AzStorageShare -Name $profileShareName -Context $storageContext
+} else {
+    write-verbose ("Share $profileShareName is present (OK)")
+}
+
+Write-Verbose ("Assigning IAM roles for access to the $profileShareName share...")
+
+if (Get-AdGroup -Identity $ShareAdminGroupName) {
+    write-verbose ("Getting ObjectID for AAD group $ShareadminGroupName...")
+    $filter = "Displayname eq '$ShareAdminGroupName'"
+    $elevatedContributorObjectId = (Get-AzureADGroup -filter $filter).ObjectId
+    if ($true -ne $?) {
+        write-warning ( "Error retrieving information for group $ShareAdminGroupName.  Check the name and try again.")
+        exit
+    }
+    write-verbose("Object ID for AAD group $ShareAdminGroupName is $elevatedContributorObjectId")
+} else {
+    write-warning "Group $ShareAdminGroupName not found in AD!"
+    exit 
+}
+
+if (get-AdGroup -Identity $ShareUserGroupName) {
+    write-verbose ("Getting ObjectID for AAD group $ShareUserGroupName...")
+    $filter = "Displayname eq '$ShareUserGroupName'"
+    $ContributorObjectId = (Get-AzureADGroup -filter $filter).ObjectId
+    if ($true -ne $?) {
+        write-warning ( "Error retrieving information for group $ShareUSerGroupName.  Check the name and try again.")
+        exit
+    }
+    write-verbose("Object ID for AAD group $ShareUserGroupName is $ContributorObjectId")
+} else {
+    write-warning "Group $ShareUserroupName not found in AD!"
+    exit 
+}
+
+# Assign SMB Contributor role to users on the share and the elevated contributor role to admins
+$scope = (Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $storageAccountRGName).Id + "/fileservices/default/fileshares/" + $profileShareName
+
+write-verbose ("Assigning role `Storage File Data SMB Share Contributor to ObjectID $ContributorObjectID...")
+$result = New-AzRoleAssignment -RoleDefinitionName "Storage File Data SMB Share Contributor" -Scope $scope -ObjectId $ContributorObjectId
+
+write-verbose ("Assigning role `Storage File Data SMB Share Elevated Contributor to ObjectID $elevatedContributorObjectID...")
+$result = New-AzRoleAssignment -RoleDefinitionName "Storage File Data SMB Share Elevated Contributor" -Scope $scope -ObjectId $elevatedContributorObjectId
+
+
+################################
+#Step 6 Map Drive with storage Key / Set NTFS permissions on root
+#################################
+#
+#  Here's what I've been using specific to WVD / FSLogix
+#
+
+#below needed for setting NTFS rights on file system - can also do manually
+$ShareName		= $profileShareName
+$drive 			= "Y:"
+$path = $Drive + "\"
+$Mapkey = $keys | where-object {$_.keyname -eq 'key1'} 
+
+################################
+# Note that the next line is for Azure Commercial - must change DNS suffix for sofisgovverign clouds
+#   Azure Gov:  .file.core.usgovcloudapi.net
+################################
+if ($isGovCloud) {
+    $MapPath = "\\"+$storageAccountName+".file.core.usgovcloudapi.net\"+$sharename
+} else {
+    $MapPath = "\\"+$storageAccountName+".file.core.windows.net\"+$sharename
+}
+
+write-verbose ("Mounting the $profileShareName share...")
+$result = new-smbmapping -LocalPath $drive -RemotePath $MapPath -UserName $storageAccountName -Password $Mapkey -Persistent $false
+
+if (!($result.status -eq "OK")) {
+    write-warning "Attempt to map path $MapPath failed."
+    exit 
+}
+
+Write-verbose ("Successfully mounted $MapPath as drive $drive")
+
+##############################################################
+#  Let's list out the ACLs on the screen so we see what's there already
+##############################################################
+$acl = Get-Acl $path
+# $acl.Access | where IsInherited -eq $false #Gets all non inherited rules.
+
+# from https://blog.netwrix.com/2018/04/18/how-to-manage-file-system-acls-with-powershell-scripts/
+##############################################################
+# clears existing users rights (for Azure Files)
+##############################################################
+write-verbose ("Clearing the ACL for the share...")
+$acl = Get-Acl $path
+$usersid = New-Object System.Security.Principal.Ntaccount ("Users")
+$acl.PurgeAccessRules($usersid)
+$acl | Set-Acl $path
+
+## used information from https://win32.io/posts/How-To-Set-Perms-With-Powershell
+
+#############################################################
+# adding my admin user - YOU DON'T NEED THIS, but might want your admin here!
+# Commented out for reference or future use
+#############################################################
+# $acl = Get-Acl $path
+# $rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $AdAdminUPN, "FullControl", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow"
+# $acl.SetAccessRule($rule)
+# $acl | Set-Acl -Path $path
+
+#############################################################
+# set " Domain Administrators / Subfolders and Files Only / Modify
+#############################################################
+write-verbose ("Adding Admins...")
+$Admins = $Domain.netbiosname + "\Domain Admins"
+$acl = Get-Acl $path
+$rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $admins, "FullControl", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow"
+$acl.SetAccessRule($rule)
+$result = $acl | Set-Acl -Path $path
+
+# set "Users / This Folder Only / Modify
+write-verbose ("Adding Authenticated Users...")
+$acl = Get-Acl $path
+$rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList "NT AUTHORITY\Authenticated Users", "Modify,Synchronize", "None", "None", "Allow"
+$result = $acl | Set-Acl -Path $path
+
+# set "Creator Owner / Subfolders and Files Only / Modify
+write-verbose ("Adding CREATOR OWNER...")
+$acl = Get-Acl $path
+$rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList "CREATOR OWNER", "Modify,Synchronize", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow"
+$acl.SetAccessRule($rule)
+$result = $acl | Set-Acl -Path $path
+
+##############################################################
+#  Uncomment to list out the resulting ACL on the share.
+##############################################################
+# $acl = Get-Acl $path
+# $acl.Access | where IsInherited -eq $false #Gets all non inherited rules.
+
+Write-Verbose ("Disconnecting from file share...")
+# Disconnect the mounted file share
+Remove-SmbMapping -LocalPath $MapPath -Force
+
+Write-Verbose ("Execution complete.")
