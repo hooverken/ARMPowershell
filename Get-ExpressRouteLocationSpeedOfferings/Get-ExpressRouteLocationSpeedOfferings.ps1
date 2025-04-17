589cb37f57df61b36c47d4bca95ec302dd4c7235
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-04-17 15:09:31 -0500

    Allow filtering to improve speed. Add some Verbose output Support multiple "bad" locations

--- Get-ExpressRouteLocationSpeedOfferings/Get-ExpressRouteLocationSpeedOfferings.ps1
+++ Get-ExpressRouteLocationSpeedOfferings/Get-ExpressRouteLocationSpeedOfferings.ps1
@@ -10,37 +10,54 @@
     Remember, you should pick the CLOSEST LOCATION TO YOUR SITE, **not** the nearest location
     to the Azure region(s) that you will be working with!
 
-.INPUTS
-    None. This script does not take any parameters.
+PARAMETER LocationName
+    The name of the location to filter on. If not specified, all locations will be returned.
+
+    Note that this is a partial match, so you can use this to filter on a substring of the location name.
 
 .EXAMPLE
     Get-ExpressRouteLocationSpeedOfferings.ps1
 
 .EXAMPLE
-    Get-ExpressRouteLocationSpeedOfferings.ps1 | Where-Object { $_.Location.StartsWith('Equinix') } | Format-Table -Auto
+    Get-ExpressRouteLocationSpeedOfferings.ps1 -Verbose
+
+.EXAMPLE
+    Get-ExpressRouteLocationSpeedOfferings.ps1 -LocationName 'Equinix' | Format-Table -Auto
 #>
 
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$LocationName = ''
+)
+
 # locations which are known to not resolve correctly are in the "bad locations" list.
-$badLocations = "CDC-Canberra-CBR20"  # Returns a 429 error for some reason
+[Array]$badLocations = @("CDC-Canberra-CBR20")  # Returns a 429 error for some reason
 
-Get-AzExpressRoutePortsLocation | ForEach-Object { 
-    $portLocation = $_.Name
+Write-Verbose "Excluding bad locations: $($badLocations -Join ', ')"
+
+Get-AzExpressRoutePortsLocation | `
+        Where-Object { $badLocations -NotContains $_.Name } | `
+        Where-Object { $_.Name -Like "*$LocationName*" -Or $LocationName.Length -eq 0 } | `
+        ForEach-Object { 
+
+        $portLocation = $_.Name
     
-    if ($badLocations -Contains $portLocation) { Return } # skip this location if it's on the naughty list
+        Write-Verbose "Checking available ports for $portLocation..."
 
-    $portSpeeds = (Get-AzExpressRoutePortsLocation -LocationName $portLocation).AvailableBandwidths
+        $portSpeeds = (Get-AzExpressRoutePortsLocation -LocationName $portLocation).AvailableBandwidths
 
-    $o = New-Object psobject
-    $o | Add-Member -MemberType NoteProperty -Name "Location" -Value $portLocation
+        $o = New-Object PSObject
+        $o | Add-Member -MemberType NoteProperty -Name "Location" -Value $portLocation
 
-    if ($portSpeeds) {
-        $o | Add-Member -MemberType NoteProperty -Name "10Gbps" -Value ($portSpeeds.OfferName.Contains("10 Gbps"))
-        $o | Add-Member -MemberType NoteProperty -Name "100Gbps" -Value ($portSpeeds.OfferName.Contains("100 Gbps"))
+        if ($portSpeeds) {
+            $o | Add-Member -MemberType NoteProperty -Name "10Gbps" -Value ($portSpeeds.OfferName.Contains("10 Gbps"))
+            $o | Add-Member -MemberType NoteProperty -Name "100Gbps" -Value ($portSpeeds.OfferName.Contains("100 Gbps"))
+        }
+        else {
+            # Port speeds list is empty so nothing available from this provider for this location
+            $o | Add-Member -MemberType NoteProperty -Name "10Gbps" -value $false
+            $o | Add-Member -MemberType NoteProperty -Name "100Gbps" -value $false
+        }
+        $o
     }
-    else {
-        # Port speeds list is empty so nothing available from this provider for this location
-        $o | Add-Member -MemberType NoteProperty -Name "10Gbps" -value $false
-        $o | Add-Member -MemberType NoteProperty -Name "100Gbps" -value $false
-    }
-    $o
-}
